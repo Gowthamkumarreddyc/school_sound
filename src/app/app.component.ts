c70688d99ed8ead5a17e9f0601583c34aafc5f30
Gowtham <gowtham.chensani@wisseninfotech.com>
2024-07-31 19:13:14 +0530

    initial code push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'school_sound';
+  subscriptionForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.subscriptionForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  onSubscribe(): void {
+    if (this.subscriptionForm.valid) {
+      // Handle the form submission
+      console.log('Form Submitted!', this.subscriptionForm.value);
+    } else {
+      // Handle validation errors
+      console.log('Form is invalid');
+    }
+  }
 }
